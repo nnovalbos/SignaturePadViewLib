d6ed62aac7e0ce3f3e3e11d370a5db1487635657
Nico <nnovalbos.inf@upsa.es>
2020-08-05 13:56:19 +0200

    IInitial project setup

---
+++ SignaturePadViewLib/SignaturePadViewLib.h
@@ -0,0 +1,19 @@
+//
+//  SignaturePadViewLib.h
+//  SignaturePadViewLib
+//
+//  Created by Nicolas Novalbos on 05/08/2020.
+//  Copyright © 2020 Nicolás Novalbos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SignaturePadViewLib.
+FOUNDATION_EXPORT double SignaturePadViewLibVersionNumber;
+
+//! Project version string for SignaturePadViewLib.
+FOUNDATION_EXPORT const unsigned char SignaturePadViewLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SignaturePadViewLib/PublicHeader.h>
+
+
---
+++ SignaturePadViewLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignaturePadViewLibExamples
+//
+//  Created by Nicolas Novalbos on 05/08/2020.
+//  Copyright © 2020 Nicolás Novalbos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
