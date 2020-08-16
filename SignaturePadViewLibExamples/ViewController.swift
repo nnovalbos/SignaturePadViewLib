047188b59e6be9deb6e2cf728386cc872bcba748
Nicolás Novalbos <nicolas.novalbos@gmail.com>
2020-08-16 11:05:15 +0200

    The test project is updated to add the new functions

--- SignaturePadViewLibExamples/ViewController.swift
+++ SignaturePadViewLibExamples/ViewController.swift
@@ -11,6 +11,8 @@ import SignaturePadViewLib
 
 class ViewController: UIViewController {
     
+    private var onlyShape: Bool = false
+    
     @IBOutlet weak var blackboard: BlackboardView!
     
     @IBOutlet weak var signatureImage: UIImageView!
@@ -19,14 +21,24 @@ class ViewController: UIViewController {
         blackboard.clear()
     }
     
+    @IBAction func AdjustChanged(_ sender: Any) {
+        let sw = sender as! UISwitch
+        
+        onlyShape = sw.isOn
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        blackboard.strokeColor = UIColor.black
     }
     
     @IBAction func getSignatureImage(_ sender: Any) {
-        if let signature = blackboard.getImage() {
+        if let signature = blackboard.getImage(adjustToShape: onlyShape) {
             signatureImage.image = signature
         }
+        
+        print(blackboard.getSVG(adjustToShape: onlyShape))
     }
 }
 
